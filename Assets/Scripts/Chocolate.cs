d95b3e9bc81c9b2b1962dc4af82eaad019d1d374
Samuel Ko <samuelsko@gmail.com>
2014-11-14 22:33:51 -0800

    pushing pls work

---
+++ Assets/Scripts/Chocolate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chocolate : MonoBehaviour {
+
+    private bool isBeingHeld;
+    private float cooldown;
+    private Vector2 location;
+
+	// Use this for initialization
+	void Start () {
+        isBeingHeld = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.collider.tag == "Player")
+        {
+            isBeingHeld = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private float velocity;
+    private bool hasChocolate;
+    private float acceleration;
+    private float accelerationMultiplier;
+    private float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+        velocity = 10.0f;
+        maxSpeed = 10.0f;
+        acceleration = 0.0f;
+        accelerationMultiplier = 0.5f;
+        hasChocolate = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(hasChocolate);
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.gameObject.transform.Translate(-Vector3.forward * velocity * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Translate(-Vector3.left * velocity * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.gameObject.transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Translate(-Vector3.right * velocity * Time.deltaTime);
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.collider.tag == "Chocolate")
+        {
+            hasChocolate = true;
+        }
+    }
+}
