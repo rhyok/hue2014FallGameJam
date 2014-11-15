dbb7f80ddd52bb899750925e0b6319a7980a9f75
Ryan <ragmanme@gmail.com>
2014-11-14 19:41:26 -0800

    Scene, HUD

---
+++ Assets/Objects/HUD.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+    Rect p1PortraitRect = new Rect(0,0, 200, 200);
+    Texture p1PortraitTexture = Resources.Load<Texture2D>("Portraits/player1.jpg");
+	void OnGUI()
+    {
+        GUI.DrawTexture(p1PortraitRect, p1PortraitTexture);
+    }
+}
