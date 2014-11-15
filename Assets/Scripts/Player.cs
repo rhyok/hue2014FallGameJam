3bb35e798afb569c7c65e030c0cb6e57af4fe48d
Samuel Ko <samuelsko@gmail.com>
2014-11-15 02:28:02 -0800

    merging

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,47 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     private float velocity;
+    private float chargeVelocity;
+    private float chargeTime;
+    private float currentChargeTime;
+    private float chargeCooldown;
     private bool hasChocolate;
     private float acceleration;
     private float accelerationMultiplier;
     private float maxSpeed;
     private Vector3 moveDir;
 
+    private bool isCharging;
+
+    private float currentChargeCooldown;
+
+    private int score;
+
 	// Use this for initialization
 	void Start () {
         velocity = 500.0f;
+        chargeVelocity = 2000.0f;
+        chargeTime = 0.25f;
+        currentChargeTime = chargeTime;
+        chargeCooldown = 5.0f;
+        currentChargeCooldown = 5.0f;
         maxSpeed = 50.0f;
         acceleration = 0.0f;
         accelerationMultiplier = 0.5f;
+        score = 0;
         hasChocolate = false;
+        isCharging = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //Debug.Log(hasChocolate);
+        Debug.Log(currentChargeTime);
+        currentChargeCooldown -= Time.deltaTime;
 
-        moveDir = Vector3.zero;
+        if (!isCharging)
+        {
+            moveDir = Vector3.zero;
+        }
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -45,20 +66,53 @@ public class Player : MonoBehaviour {
             moveDir += Vector3.right;
         }
 
-        this.gameObject.rigidbody.velocity = moveDir * velocity * Time.deltaTime;
+        if (isCharging)
+        {
+            currentChargeTime -= Time.deltaTime;
+
+            if (currentChargeTime <= 0.0f)
+            {
+                currentChargeTime = chargeTime;
+                isCharging = false;
+                this.gameObject.rigidbody.velocity = moveDir * velocity * Time.deltaTime;
+            }
+            else
+            {
+                this.gameObject.rigidbody.velocity = moveDir * chargeVelocity * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                currentChargeCooldown = chargeCooldown;
+
+                isCharging = true;
+                this.gameObject.rigidbody.velocity = moveDir * chargeVelocity * Time.deltaTime;
+            }
+            else
+            {
+                this.gameObject.rigidbody.velocity = moveDir * velocity * Time.deltaTime;
+
+            }
+        }
+
+        
 	}
 
     void OnTriggerEnter(Collider col)
     {
-        string tag = col.collider.tag;
-        if (tag == "Chocolate")
+        string name = col.collider.name;
+        if (name == "Chocolate")
         {
             hasChocolate = true;
             Destroy(col.gameObject);
         }
-        else if (tag == "Senpai")
+        else if (name == "Senpai" && hasChocolate)
         {
             hasChocolate = false;
+            score++;
+            Debug.Log(this.name + " has been noticed!");
         }
     }
 }
