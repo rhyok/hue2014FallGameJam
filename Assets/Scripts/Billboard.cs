81de5800a855cfe3fc84521f40bc57a442b8fa86
Ryan <ragmanme@gmail.com>
2014-11-15 16:25:31 -0800

    Billboarding,l Podium, Player sprites

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+	void Update () {
+        Vector3 oppositeCamera = transform.position - Camera.main.transform.position;
+        Quaternion faceCamera = Quaternion.LookRotation(oppositeCamera);
+        Vector3 euler = faceCamera.eulerAngles;
+        euler.y = 0f;
+        faceCamera.eulerAngles = euler;
+        transform.rotation = faceCamera;
+	}
+}
