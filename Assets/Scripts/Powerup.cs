2124a1a582e3ce70fdeb7f3e7ba8426d9bc473a6
Samuel Ko <samuelsko@gmail.com>
2014-11-15 17:25:54 -0800

    aweirjteiow

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+    protected GameObject powerup;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Toast.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toast : Powerup {
+    
+	// Use this for initialization
+	void Start () {
+        powerup = Resources.Load("Objects/Toast") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
